967e2391a92ca01cbf9cfb72a772caff63773796
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2019-08-28 11:03:20 -0300

    Listo

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var cartaOcupada = null;
 let cartasNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 var imagenes = ['chrome.svg', 'facebook.svg', 'firefox.svg', 'google-icon.svg', 'html-5.svg', 'instagram-icon.svg', 'internetexplorer.svg', 'javascript.svg', 'opera.svg', 'chrome.svg', 'facebook.svg', 'firefox.svg', 'google-icon.svg', 'html-5.svg', 'instagram-icon.svg', 'internetexplorer.svg', 'javascript.svg', 'opera.svg'];
 
-
 function Click(id) {
 
     if (cartaOcupada == null && listo) {
@@ -27,34 +26,28 @@ function Click(id) {
 
             imagen2 = document.getElementById(id + 20);
             imagen2.className = "mostrar";
-
             listo = false;
-
             cartaOcupada = null;
-            contClick++;
-            document.getElementById('intentos').innerHTML = contClick;
-
             let valor = carta1.id - carta2.id;
-
             valor = Math.abs(valor);
-
             if (valor == 10) {
 
                 listo = true;
                 contAcierto++;
                 if (contAcierto > 8) {
-                    alert("Ganaste despues de "+ contClick +" intentos");
+                    alert("Ganaste despues de " + contClick + " intentos");
                     Reiniciar();
                 }
             }
             else {
                 setTimeout(function () { Ocultar(imagen1, imagen2); }, 800);
+                contClick++;
             }
+            document.getElementById('intentos').innerHTML = contClick;
         }
     }
 
 }
-
 
 function mezclar() {
 
@@ -74,13 +67,10 @@ function mezclar() {
         if (tamaño > 0) {
             tamaño--;
         }
-
     }
-
 }
 
 function Ocultar(img1, img2) {
-
     img1.className = "ocultar";
     img2.className = "ocultar";
     listo = true;
@@ -89,7 +79,6 @@ function Ocultar(img1, img2) {
 function Reiniciar() {
 
     document.location.reload();
-
     contClick = 0;
     contAcierto = 0;
     carta1 = null;
@@ -98,9 +87,7 @@ function Reiniciar() {
     imagen2 = null;
     listo = true;
     cartaOcupada = null;
-
     mezclar();
-
 }
 
 mezclar();
